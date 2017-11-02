0e6f9cdecd147d86219b4aabee9758f061850479
Anurag Mishra <anuragmishracse@gmail.com>
2017-11-03 00:51:20 +0530

    Remove syntax errors

--- smic/smic.py
+++ smic/smic.py
@@ -18,10 +18,11 @@ import pandas as pd
 import numpy as np
 import cv2, os, sys
 from tqdm import tqdm
+from collections import defaultdict
 
 class SMIC():
 
-	def __init__(color = 0, dim = 256, gpu=0):
+	def __init__(self, color = 1, dim = 256, gpu=0):
 		self.color = color
 		self.dim = dim
 		self.gpu = gpu
@@ -34,28 +35,27 @@ class SMIC():
 		self.train_labels = []
 		self.num_classes = -1
 		self.hyperparameters = {}
-		self.transfer_models = {'vgg16' : VGG16, 'vgg19' : VGG19, 'resnet50' : ResNet50, 'inception_v3' : inception_v3}
-		self.optimizers = {'sgd' : SGD, 'rmsprop' : RMSprop, 'adam' : Adam}
+		self.transfer_models = {'vgg16' : VGG16, 'vgg19' : VGG19, 'resnet50' : ResNet50, 'inception_v3' : InceptionV3}
+		self.optimizers = {'sgd' : 'SGD', 'rmsprop' : 'RMSprop', 'adam' : 'Adam'}
 		self.layers = {'dense' : Dense, 'dropout' : Dropout}
 
-	def read_image(path):
+	def read_image(self, path):
 		image = cv2.imread(path, self.color)
 		image = cv2.resize(image, (self.dim, self.dim))
 		iamge = np.array(image, np.float32) / 255.
 		return image
 
-	def prepare_train_data(data_location):
+	def prepare_train_data(self, data_location):
 		try:
-			train = pd.read_csv(os.path.join(data_location,'trainImages.csv'))
+			train = pd.read_csv(os.path.join(data_location,'trainLabels.csv'))
+			for image_name in tqdm(train['image_id'].values):
+				try:
+					self.train_images.append(self.read_image(os.path.join(data_location, self.TRAIN_PATH, image_name)+'.png'))
+				except Exception as e:
+					print("Error reading image: " + repr(e))
 		except:
 			print("Error: Invalid location/ File not present.")
-		for image_name in tqdm(train['image_id'].values):
-			try:
-		    	self.train_images.append(self.read_image(os.path.join(data_location, self.TRAIN_PATH, image_name)))
-		    except Exception as e:
-		    	print("Error reading image: " + repr(e))
-		    	print("Breaking.")
-		    	break
+			exit()
 
 		labels = train['label'].tolist()
 		self.num_classes = len(set(labels))
@@ -63,34 +63,62 @@ class SMIC():
 		self.rev_label_map = {v:k for v,k in enumerate(set(labels))}
 		self.train_labels = np.asarray([self.label_map[label] for label in labels])
 
-	def prepare_test_data():
+	def prepare_test_data(self):
 		pass
 
-	def create_model(hyperparameters):
-		base_model = self.transfer_models(hyperparameters['transfer_model'])(weights='imagenet', include_top=False, input_shape=(self.dim, self.dim, 3))
+	def create_model(self, hyperparameters):
+		base_model = self.transfer_models[hyperparameters['transfer_model']](weights='imagenet', include_top=False, input_shape=(self.dim, self.dim, 3))
 		for layer in base_model.layers:
-    		layer.trainable=False
+			layer.trainable=False
 		classifier = Flatten()(base_model.output)
 		for layer_param in hyperparameters['top_layers']:
 			classifier = self.layers[layer_param[0]](layer_param[1], activation=layer_param[2])(classifier)
 		classifier = Dense(self.num_classes, activation='softmax')(classifier)
 		model = Model(base_model.input, classifier)
 
-		model.compile(loss='categorical_crossentropy', optimizer= self.optimizers[hyperparameters[optimizer]],
-              metrics=['accuracy'])
+		model.compile(loss='categorical_crossentropy', optimizer = self.optimizers[hyperparameters['optimizer']], metrics=['accuracy'])
+		return model
 
-	def search_optimal_hyperparameters(samples_per_class = 50, check_epochs = 10):
-		labels_catgorical = to_categorical(self.train_labels)
-		return hyperparameters
+	def search_optimal_hyperparameters(self, samples_per_class = 50, check_epochs = 10):
+		search_result = {}
 
-	# hyperparameters = {
-	# 'transfer_model':'resnet50',
-	# 'top_layers':[(),()],
-	# 'optimization_algo':'SGD',
-	# }
+		sample_train_images=[]
+		sample_labels_catgorical=[]
 
-	def fit(hyperparameters, epochs, batch_size, fine_tune = False):
-		labels_catgorical = to_categorical(self.train_labels)
+		class_image_dict = defaultdict(list)
+		for ind in range(len(self.train_labels)):
+			class_image_dict[self.train_labels[ind]].append(self.train_images[ind])
+
+		for class_name in class_image_dict.keys():
+			class_images = class_image_dict[class_name][:samples_per_class]
+			sample_train_images.extend(class_images)
+			sample_labels_catgorical.extend([class_name]*len(class_images))
+
+		sample_labels_catgorical = to_categorical(sample_labels_catgorical)
+
+		for transfer_model in self.transfer_models.keys():
+			for optimizer in self.optimizers.keys():
+				layers=[]
+				for layer_count in range(1,2):
+					layers.append(['dense', 512, 'relu']) 
+					hyperparameters={'transfer_model' : transfer_model, 'optimizer' : optimizer, 'top_layers' : layers}
+					model = self.create_model(hyperparameters)
+					history = model.fit(np.asarray(sample_train_images), np.asarray(sample_labels_catgorical), batch_size=32, epochs = check_epochs, validation_split = 0.1)
+					search_result[str(hyperparameters)]=[history['acc'][-1], history['val_acc'][-1]]
+
+		print search_result
+
+		for hyperparameters, results in search_result:
+			if results[1] > results[0]*1.05:
+				del search_result[hyperparameters]
+
+		search_result = sorted(search_result.items(), key = lambda x: x[1], reverse=True)
+
+		return dict(search_result[0][0])
+
+
+	def fit(self, hyperparameters, epochs, batch_size, fine_tune = False):
+		labels_categorical = to_categorical(self.train_labels)
 		self.model = create_model(hyperparameters)
 		history = self.model.fit(np.asarray(self.train_images), np.asarray(labels_catgorical), batch_size=batch_size, epochs = epochs, validation_split = 0.1)
 
@@ -102,17 +130,17 @@ class SMIC():
 			history.extend(history_fine)
 		return history
 
-	def predict(image):
+	def predict(self, image):
 		prediction = self.model.predict(np.asarray([image]))
 		prediction = np.argmax(prediction, axis=1)
 		return self.rev_label_map[prediction]
 
-	def visualize(summary=False):
+	def visualize(self, summary=False):
 		if summary:
 			print self.model.summary()
 
-	def save(path):
+	def save(self, path):
 		self.model.save(path)
 
-	def load(path):
+	def load(self, path):
 		self.model = load_model(path)
